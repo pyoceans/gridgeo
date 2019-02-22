3f6a03716e493020c45ba623fe90f32352a12eb7
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-02-22 17:48:49 +0000

    Update docs after building Travis build 101 of pyoceans/gridgeo
    The docs were built from the branch 'master' against the commit
    0abdea697418f16e789abe713b157d7dad0d5479.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pyoceans/gridgeo/jobs/497155958.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.7/site-packages/doctr/__main__.py deploy --sync --no-require-master --built-docs docs/build/html docs-master

--- docs-master/_static/documentation_options.js
+++ docs-master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0+22.g04e1e23',
+    VERSION: '1.4.0+24.g0abdea6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
